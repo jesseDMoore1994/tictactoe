7b709271a31614380bfad34327f7e1e916befa75
Jesse Moore <jesse@jessemoore.dev>
2022-12-06 00:39:34 -0600

    some minor refactors

--- tictactoe.c
+++ tictactoe.c
@@ -6,6 +6,26 @@
 #define BOARDSIZE 3
 
 
+char player_print(int pid) {
+    switch(pid) {
+        case 0:
+            return '_';
+        case 1:
+            return 'X';
+        case 2:
+            return 'O';
+        default:
+            return '?';
+    }
+}
+
+
+int player_next(int pid) {
+    if (pid == 2) return 1;
+    return pid + 1;
+};
+
+
 int* board_create() {
     int* board = (int*)malloc(
         ((BOARDSIZE) * (BOARDSIZE)) * sizeof(int)
@@ -18,21 +38,12 @@ int* board_create() {
 
 
 void board_print(int* board) {
+    printf("Current board: \n");
     printf("Col:   0 1 2\n");
     for(int i = 0; i < (BOARDSIZE); i++) {
         printf("Row %d: ", i);
         for(int j = 0; j < (BOARDSIZE); j++) {
-            switch(board[(i*(BOARDSIZE) + j)]) {
-                case 0:
-                    printf("_ ");
-                    break;
-                case 1:
-                    printf("X ");
-                    break;
-                case 2:
-                    printf("O ");
-                    break;
-            }
+            printf("%c ", player_print(board[(i*(BOARDSIZE) + j)]));
         }
         printf("\n");
     }
@@ -53,28 +64,7 @@ void board_destroy(int* board) {
 }
 
 
-char player_print(int pid) {
-    switch(pid) {
-        case 1:
-            return 'X';
-        case 2:
-            return 'O';
-        default:
-            return '?';
-    }
-}
-
-
-int player_next(int pid) {
-    if (pid == 1) return 2;
-    return 1;
-};
-
-
 int game_get_winner(int* board) {
-    // 0 1 2
-    // 3 4 5
-    // 6 7 8
     if (board[0] != 0 && board[0] == board[1] && board[1] == board[2]) return board[0];
     if (board[3] != 0 && board[3] == board[4] && board[4] == board[5]) return board[3];
     if (board[6] != 0 && board[6] == board[7] && board[7] == board[8]) return board[6];
@@ -87,29 +77,53 @@ int game_get_winner(int* board) {
 }
 
 
+struct PlayerInput {
+    int xpos;
+    int ypos;
+};
+typedef struct PlayerInput PlayerInput;
+
+
+PlayerInput* game_create_player_input() {
+    PlayerInput* item = malloc(sizeof(PlayerInput));
+    printf("Enter your play: ");
+    int res = scanf("%d %d", &item->xpos, &item->ypos);
+    assert(res==2);
+    return item;
+}
+
+
+void game_destroy_player_input(PlayerInput* i) {
+    free(i);
+}
+
+
+bool player_take_turn(int* board, int player) {
+    PlayerInput* i = game_create_player_input();
+    bool res = board_set(board, player, i->xpos, i->ypos);
+    game_destroy_player_input(i);
+
+    if(!res) {
+        printf("Setting positon failed! (did someone play there already?)");
+        return false;
+    }
+    return true;
+}
+
+
 int game_play(int* board, int player) {
-    printf("Current board: \n");
     board_print(board);
-    
     printf("Current Player: %c\n", player_print(player));
 
-    printf("Enter your play: ");
-    int xpos, ypos;
-    int res = scanf("%d %d", &xpos, &ypos);
-    assert(res==2);
-    printf("\n\n");
-
-    if(!board_set(board, player, xpos, ypos)) {
-        printf("Setting positon failed! (did someone play there already?)");
+    bool res = player_take_turn(board, player);
+    if(!res) {
         return player;
     }
-
     return player_next(player);
 }
 
 
 void game_show_winner(int* board) {
-    printf("Current board: \n");
     board_print(board);
     printf("Winner! %c\n", player_print(game_get_winner(board)));
 }
@@ -118,11 +132,9 @@ void game_show_winner(int* board) {
 int main() {
     int* board = board_create();
     int current_player = 1;
-
     while(!game_get_winner(board)) {
         current_player = game_play(board, current_player);
     };
     game_show_winner(board);
-
     board_destroy(board);
 }
